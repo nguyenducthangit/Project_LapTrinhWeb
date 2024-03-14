58441056a4a2b814538e060581d1a7c0b9bd2424
nguyenducthangit <nthang0987@gmail.com>
2024-03-15 00:07:08 +0700

    new file

---
+++ WebApplication3/Models/UserMV.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class UserMV
+    {
+        public UserMV()
+        {
+            Company = new CompanyMV();
+        }
+        public int UsersID { get; set; }
+        public int UserTypeID { get; set; }
+        [Required(ErrorMessage ="Required*")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Required*")]
+
+
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Required*")]
+
+
+        public string EmailAddress { get; set; }
+        [Required(ErrorMessage = "Required*")]
+
+
+        public bool AreYouProvider { get; set; }
+        [Required(ErrorMessage = "Required*")]
+
+        public string ContactNO { get; set; }
+
+
+        public CompanyMV Company { get; set; }
+    }
+}
